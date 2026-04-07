7d235b30880c8e7317256c90e1e7f1b8b10a7d9c
yaqbx <y-aqb@o2.pl>
2026-04-07 09:46:43 +0200

    terraform vpc

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,8 +1,8 @@
-# resource "aws_vpc" "main" {
-#   cidr_block       = "10.0.1.0/24"
-#   instance_tenancy = "default"
-
-#   tags = {
-#     Name = "main"
-#   }
-# }
+module "vpc" {
+  source               = "./modules/aws_vpc"
+  region               = var.aws_region
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  enable_nat_gateway   = true
+}
