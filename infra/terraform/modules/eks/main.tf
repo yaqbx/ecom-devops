e337af7f4baf2ddd1c1d64f0cdb2a750dbd5c9e5
yaqbx <33007344+yaqbx@users.noreply.github.com>
2026-08-09 18:47:39 +0200

    added access entry for local admin in terraform eks module (#65)

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -193,3 +193,17 @@ resource "aws_eks_access_policy_association" "app_build" {
     type = "cluster"
   }
 }
+resource "aws_eks_access_entry" "admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::163841615263:root"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_eks_access_entry.admin.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  access_scope {
+    type = "cluster"
+  }
+}
